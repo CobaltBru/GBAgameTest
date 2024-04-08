940c4b3042192186b74d582329967e57fa014ee0
CobaltBru <97279958+CobaltBru@users.noreply.github.com>
2024-04-08 15:48:08 +0900

    .
    .

--- something/source/main.c
+++ something/source/main.c
@@ -2,82 +2,79 @@
 #include <stdio.h>
 #include <tonc.h>
 
-// 선분을 그리는 함수
-void draw_line(int x0, int y0, int x1, int y1, u16 color)
-{
-    m3_line(x0, y0, x1, y1, color);
-}
+// 정육면체 정점 정의
+VECTOR cube_vertices[] = {
+    {-30, -30, -30}, {30, -30, -30}, {30, 30, -30}, {-30, 30, -30},
+    {-30, -30,  30}, {30, -30,  30}, {30, 30,  30}, {-30, 30,  30}
+};
 
-// 삼각형 내부를 채우는 함수
-void fill_triangle(int x0, int y0, int x1, int y1, int x2, int y2, u16 color)
-{
-    // y0, y1, y2를 정렬하여 y0 <= y1 <= y2 순서가 되도록 함
-    if(y0 > y1) {
-        SWAP(y0, y1);
-        SWAP(x0, x1);
-    }
-    if(y1 > y2) {
-        SWAP(y2, y1);
-        SWAP(x2, x1);
-    }
-    if(y0 > y1) {
-        SWAP(y0, y1);
-        SWAP(x0, x1);
-    }
+// 정육면체 면을 구성하는 정점 인덱스
+u16 cube_faces[][4] = {
+    {0, 1, 2, 3}, {1, 5, 6, 2}, {5, 4, 7, 6},
+    {4, 0, 3, 7}, {3, 2, 6, 7}, {4, 5, 1, 0}
+};
 
-    // 상단 삼각형 채우기
-    int dy0 = y1 - y0;
-    int dx0 = x1 - x0;
-    int dy1 = y2 - y0;
-    int dx1 = x2 - x0;
+void draw_cube(VECTOR *vertices, int color) {
+    int focalLength = 40; // 초점 거리
+    int centerX = 120, centerY = 80; // 화면 중앙
 
-    float dxdy0 = dx0 / (float)dy0;
-    float dxdy1 = dx1 / (float)dy1;
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 4; j++) {
+            VECTOR *v1 = &vertices[cube_faces[i][j]];
+            VECTOR *v2 = &vertices[cube_faces[i][(j + 1) % 4]];
 
-    float x02 = x0;
-    float x01 = x0;
-    
-    for(int y=y0; y<y1; y++)
-    {
-        draw_line((int)x01, y, (int)x02, y, color);
-        x01 += dxdy0;
-        x02 += dxdy1;
-    }
+            // Z값을 고려하여 3D 좌표를 2D 스크린 좌표로 변환
+            int x1 = ((v1->x * focalLength) / (v1->z + focalLength + 100)) + centerX;
+            int y1 = ((v1->y * focalLength) / (v1->z + focalLength + 100)) + centerY;
+            int x2 = ((v2->x * focalLength) / (v2->z + focalLength + 100)) + centerX;
+            int y2 = ((v2->y * focalLength) / (v2->z + focalLength + 100)) + centerY;
 
-    // 하단 삼각형 채우기
-    dy0 = y2 - y1;
-    dx0 = x2 - x1;
-
-    dxdy0 = dx0 / (float)dy0;
-    x01 = x1;
-    
-    for(int y=y1; y<=y2; y++)
-    {
-        draw_line((int)x01, y, (int)x02, y, color);
-        x01 += dxdy0;
-        x02 += dxdy1;
+            // m3_line 함수를 사용하여 화면에 직선 그리기
+            m3_line(x1, y1, x2, y2, color);
+        }
     }
 }
 
-int main()
-{
-	
-	REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+// 3D 벡터를 회전시키는 함수 (Y축, X축, Z축 순서)
+void rotate_vector(VECTOR *v, int angleX, int angleY, int angleZ) {
+    int s, c;
+    VECTOR temp = *v;
 
-	// 삼각형의 꼭지점 좌표 설정
-    int x0 = 0, y0 = 0;  // 꼭지점 1
-    int x1 = 50, y1 = 0;  // 꼭지점 2
-    int x2 = 50, y2 = 50;  // 꼭지점 3
+    // Y축 회전
+    s = lu_sin(angleY); c = lu_cos(angleY);
+    temp.x = (v->x * c - v->z * s) >> 14;
+    temp.z = (v->x * s + v->z * c) >> 14;
 
-    // 삼각형 색상 설정
-    u16 color = CLR_YELLOW;  // 노란색
+    // X축 회전
+    s = lu_sin(angleX); c = lu_cos(angleX);
+    v->y = (temp.y * c - temp.z * s) >> 14;
+    v->z = (temp.y * s + temp.z * c) >> 14;
 
-    // 삼각형 채우기
-    fill_triangle(x0, y0, x1, y1, x2, y2, color);
-	m3_rect(20, 20, 100, 60, RGB15(31, 0, 0)); // 시작점 (20, 20), 폭 100, 높이 60인 빨간색 사각형 그리기
-    // 메인 루프
-    while(1)
-    {
-        // 아무 작업도 하지 않음
+    // Z축 회전 (화면에 더 잘 표시되도록 추가)
+    s = lu_sin(angleZ); c = lu_cos(angleZ);
+    v->x = (temp.x * c - v->y * s) >> 14;
+    v->y = (temp.x * s + v->y * c) >> 14;
+}
+// 메인 함수
+int main() {
+    REG_DISPCNT = DCNT_MODE3 | DCNT_BG2; // Mode 3 설정
+
+    int angleX = 0, angleY = 0, angleZ = 0;
+
+    while(1) {
+        vid_vsync();
+        m3_fill(CLR_BLACK); // 화면 클리어
+
+        VECTOR rotated_vertices[8];
+        for(int i = 0; i < 8; i++) {
+            rotated_vertices[i] = cube_vertices[i];
+            rotate_vector(&rotated_vertices[i], angleX, angleY, angleZ);
+        }
+
+        draw_cube(rotated_vertices, RGB15(31,31,31)); // 정육면체 그리기
+
+        angleX += 0x68;
+        angleY += 0x88;
+        angleZ += 0x48;
     }
 }
